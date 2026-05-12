04580300120ee7f07d007c0a705a7fd0efe0c6b2
Araf05 <aracelifernandez.3006@gmail.com>
2026-05-11 22:37:48 -0300

    Agrego en Pedido.java: atributos y metodos

--- src/com/techlab/ecommerce/model/Pedido.java
+++ src/com/techlab/ecommerce/model/Pedido.java
@@ -1,5 +1,64 @@
 package com.techlab.ecommerce.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Time;
+import java.sql.Timestamp;
+
 public class Pedido {
-    
+    private int id;
+    private Timestamp fecha = new Timestamp(System.currentTimeMillis());
+    private List<LineaPedido> lineasPedido = new ArrayList<>();
+    private double costoTotal;
+
+    /// Constructor
+    public Pedido() {
+    }
+
+    /// Getters
+    public int getId() {
+        return id;
+    }
+
+    public Timestamp getFecha() {
+        return fecha;
+    }
+
+    public List<LineaPedido> getLineasPedido() {
+        return lineasPedido;
+    }
+
+    public double getCostoTotal() {
+        return costoTotal;
+    }
+
+    /// Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFecha() {
+        fecha = new Timestamp(System.currentTimeMillis());
+    }
+
+    public void agregarLinea(LineaPedido linea) {
+        this.lineasPedido.add(linea);
+        recalcularTotal();
+    }
+
+    private void recalcularTotal() {
+        costoTotal = 0;
+
+        for (LineaPedido linea : lineasPedido) {
+            costoTotal += linea.calcularSubtotal();
+        }
+    }
+
+    /// Override
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
 }
